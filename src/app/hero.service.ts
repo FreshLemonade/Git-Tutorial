276db8ca65c9c6e9dd3892460bf3b473d1165037
FreshLemonade <seky.dsbv@gmail.com>
2017-09-11 15:13:43 +0200

    starting with services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './models/hero-model';
+import { HEROES } from './mock-heroes';
+
+
+@Injectable()
+export class HeroService {
+
+    getHeroes(): Hero[] {
+        return HEROES;
+      }
+
+}
