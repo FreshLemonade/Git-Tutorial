fa0c2e3f5dacd490a2f26a72ad1e18faca082711
FreshLemonade <seky.dsbv@gmail.com>
2017-09-11 13:45:50 +0200

    blah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Hero } from './models/hero-model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: '<h1>{{title}}</h1><h2> {{hero.name}} details!</h2>'
 })
+
 export class AppComponent {
-  title = 'app';
+title = 'Tour of Heroes';
+hero: Hero = {
+  id: 1, 
+  name: 'Windstorm'  
+  };
 }
