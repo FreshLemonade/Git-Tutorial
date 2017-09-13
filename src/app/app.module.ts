6c19697e3fad702db31d4001e551a31c03e7c385
FreshLemonade <seky.dsbv@gmail.com>
2017-09-13 10:46:50 +0200

    HTTP 1st part done
    fixed angular-in-memory-web-api error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent }     from './heroes.component';
@@ -11,6 +12,10 @@ import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 
 import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 
 
@@ -25,6 +30,8 @@ import { AppRoutingModule }     from './app-routing.module';
     BrowserModule,
     FormsModule,
     RouterModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path: 'dashboard',
