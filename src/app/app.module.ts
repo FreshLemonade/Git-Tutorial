e660b36c1ba574e87546ffedd11833022e09cf74
FreshLemonade <seky.dsbv@gmail.com>
2017-09-13 11:22:56 +0200

    Tutorial completed
    Implemented search, all working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -24,7 +25,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     AppComponent,
     HeroDetailComponent,
     DashboardComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
